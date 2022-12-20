7ae44fb9f80d8959efc310d54843766f4cea0862
arjoonn sharma <arjoonn@midpathsoftware.com>
2022-12-21 00:16:40 +0530

    run CI

--- cicd/cicd.py
+++ cicd/cicd.py
@@ -2,17 +2,21 @@ from jaypore_ci import jci
 
 with jci.Pipeline(image="Will set later", timeout=15 * 60) as p:
     p.image = image = f"jaypore_image_{p.remote.sha}"
-    p.in_sequence(
-        p.job(
-            f"docker build -t {image} .",
-            image="arjoonn/jaypore_ci:latest",
-            name="Docker build",
-        ),
-        p.in_parallel(
-            p.job("pwd", name="Pwd"),
-            p.job("tree", name="Tree"),
-            p.job("python3 -m black --check .", name="Black"),
-            p.job("python3 -m pylint jaypore_ci/ tests/", name="PyLint"),
-            p.job("python3 -m pytest tests/", name="PyTest"),
-        ),
-    ).should_pass()
+    # Build image
+    p.job(
+        f"docker build -t {image} .",
+        image="arjoonn/jaypore_ci:latest",
+        name="Docker build",
+    )
+    # Run jobs
+    p.job("pwd", name="Pwd", depends_on=["Docker build"])
+    p.job("tree", name="Tree", depends_on=["Docker build"])
+    p.job("python3 -m black --check .", name="Black", depends_on=["Docker build"])
+    p.job(
+        "python3 -m pylint jaypore_ci/ tests/",
+        name="PyLint",
+        depends_on=["Docker build"],
+    )
+    p.job("python3 -m pytest tests/", name="PyTest", depends_on=["Docker build"])
+    # Run the pipe
+    p.run()
