7eefe09825d4d62b86f7e1a1b464a7cc441362e6
arjoonn sharma <arjoonn@midpathsoftware.com>
2022-12-23 20:28:28 +0530

    x

--- jaypore_ci/jci.py
+++ jaypore_ci/jci.py
@@ -294,7 +294,12 @@ class Pipeline:  # pylint: disable=too-many-instance-attributes
         job.
         """
         color = self.get_status_color()
-        logo = f"<img src='https://www.jayporeci.in/_static/colors/{color}.png' alt='Jaypore CI' width='25px' style='vertical-align:middle;' />"
+        logo = f"""
+            <img
+            src='https://www.jayporeci.in/_static/colors/{color}.png'
+            alt='{color}'
+            width='25px'
+            />"""
         return f"""
 <details>
     <summary id='jaypore_ci'>{logo}:  {self.remote.sha[:10]}</summary>
