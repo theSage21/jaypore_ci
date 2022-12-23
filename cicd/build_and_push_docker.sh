33d776a75a54dd83cea8f15eaaacf96c68c6451e
arjoonn sharma <arjoonn@midpathsoftware.com>
2022-12-23 11:51:08 +0530

    x

--- cicd/build_and_push_docker.sh
+++ cicd/build_and_push_docker.sh
@@ -7,5 +7,5 @@ set -o pipefail
 source cicd/set_env.sh
 docker login -u arjoonn -p=$DOCKER_PWD
 docker build --target $1 -t $1:latest .
-docker tag  $1:latest arjoonn/$1:latest
+docker tag $1:latest arjoonn/$1:latest
 docker push arjoonn/$1:latest
