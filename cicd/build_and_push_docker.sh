05d92444b6cd61688eb33bff718594aa1c67dfe6
arjoonn <arjoonn@noreply.localhost>
2022-12-23 06:50:47 +0000

    push_image (!11)
    Branch auto created by JayporeCI
    
    <details>
        <summary>JayporeCi: 🟢 33d776a75a</summary>
    
    ```mermaid
    flowchart TB
    
                subgraph Pipeline
                    direction TB
    
                end
    
                subgraph Docker
                    direction TB
    
                    s_Docker(( )) -.-> Docker_0(JciEnv):::passed
                    s_Docker(( )) -.-> Docker_1(Jci):::passed
                end
    
                subgraph Jobs
                    direction TB
    
                    s_Jobs(( )) -.-> Jobs_0(black):::passed
                    s_Jobs(( )) -.-> Jobs_1(pytest):::passed
                    s_Jobs(( )) -.-> Jobs_2(pylint):::passed
                end
    
                subgraph Publish
                    direction TB
    
                    s_Publish(( )) -.-> Publish_0(DockerHubJci):::passed
                    s_Publish(( )) -.-> Publish_1(PublishDocs):::passed
                    s_Publish(( )) -.-> Publish_2(DockerHubJcienv):::passed
                end
    
                Pipeline ---> Docker
    
                Docker ---> Jobs
    
                Jobs ---> Publish
    
                classDef pending fill:#aaa, color:black, stroke:black,stroke-width:2px,stroke-dasharray: 5 5;
                classDef skipped fill:#aaa, color:black, stroke:black,stroke-width:2px;
                classDef assigned fill:#ddd, color:black, stroke:black,stroke-width:2px;
                classDef running fill:#bae1ff,color:black,stroke:black,stroke-width:2px,stroke-dasharray: 5 5;
                classDef passed fill:#88d8b0, color:black, stroke:black;
                classDef failed fill:#ff6f69, color:black, stroke:black;
                classDef timeout fill:#ffda9e, color:black, stroke:black;
    ```
    
    Co-authored-by: arjoonn sharma <arjoonn@midpathsoftware.com>
    Reviewed-on: https://gitea.midpathsoftware.com/midpath/jaypore_ci/pulls/11

--- cicd/build_and_push_docker.sh
+++ cicd/build_and_push_docker.sh
@@ -6,6 +6,6 @@ set -o pipefail
 
 source cicd/set_env.sh
 docker login -u arjoonn -p=$DOCKER_PWD
-docker build -t $1:latest .
-docker tag  $1:latest arjoonn/$1:latest
+docker build --target $1 -t $1:latest .
+docker tag $1:latest arjoonn/$1:latest
 docker push arjoonn/$1:latest
