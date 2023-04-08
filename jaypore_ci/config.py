46ea23460c556e2a452b931d087496a69887b931
demo <demo@example.com>
2023-04-08 10:08:33 +0530

    tag 32

--- jaypore_ci/config.py
+++ jaypore_ci/config.py
@@ -54,8 +54,7 @@ class Const(NamedTuple):
     repo_root: str = os.environ.get("REPO_ROOT")
     repo_sha: str = os.environ.get("REPO_SHA")
     env: str = os.environ.get("ENV")
-    # image:str = f'arjoonn/jci:{version}'
-    image: str = "jci"
+    image: str = f"arjoonn/jci:{version}"
 
 
 const = Const()
