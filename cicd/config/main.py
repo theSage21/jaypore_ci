7dbd44979212036c57da8afa565c98bb71cd34a8
demo <demo@example.com>
2023-05-22 12:55:44 +0530

    do not do anything in main

--- cicd/config/main.py
+++ cicd/config/main.py
@@ -20,36 +20,41 @@ def parse_commit(repo):
     return Should(**config)
 
 
-with jci.Pipeline() as p:
-    should = parse_commit(p.repo)
-    jcienv = f"jcienv:{p.repo.sha}"
-    with p.stage("build_and_test"):
-        p.job("JciEnv", f"docker build  --target jcienv -t jcienv:{p.repo.sha} .")
-        p.job(
-            "Jci",
-            f"docker build  --target jci -t jci:{p.repo.sha} .",
-            depends_on=["JciEnv"],
-        )
-        kwargs = dict(image=jcienv, depends_on=["JciEnv"])
-        p.job("black", "python3 -m black --check .", **kwargs)
-        p.job("pylint", "python3 -m pylint jaypore_ci/ tests/", **kwargs)
-        p.job("pytest", "bash cicd/run_tests.sh", image=jcienv, depends_on=["JciEnv"])
-        p.job(
-            "install_test",
-            "bash cicd/test_installation.sh",
-            image=jcienv,
-            depends_on=["JciEnv"],
-        )
-
-    with p.stage("Debug", image=jcienv):
-        p.job("PublishDocs", f"bash cicd/build_and_publish_docs.sh {p.remote.branch}")
-
-    if should.release:
-        with p.stage("Publish", image=jcienv):
-            p.job("DockerHubJcienv", "bash cicd/build_and_push_docker.sh jcienv")
-            p.job("DockerHubJci", "bash cicd/build_and_push_docker.sh jci")
+def main():
+    with jci.Pipeline() as p:
+        should = parse_commit(p.repo)
+        jcienv = f"jcienv:{p.repo.sha}"
+        with p.stage("build_and_test"):
+            p.job("JciEnv", f"docker build  --target jcienv -t jcienv:{p.repo.sha} .")
             p.job(
-                "PublishDocs",
-                f"bash cicd/build_and_publish_docs.sh {p.remote.branch}",
+                "Jci",
+                f"docker build  --target jci -t jci:{p.repo.sha} .",
+                depends_on=["JciEnv"],
             )
-            p.job("PublishPypi", "bash cicd/build_and_push_pypi.sh")
+            kwargs = dict(image=jcienv, depends_on=["JciEnv"])
+            p.job("black", "python3 -m black --check .", **kwargs)
+            p.job("pylint", "python3 -m pylint jaypore_ci/ tests/", **kwargs)
+            p.job(
+                "pytest", "bash cicd/run_tests.sh", image=jcienv, depends_on=["JciEnv"]
+            )
+            p.job(
+                "install_test",
+                "bash cicd/test_installation.sh",
+                image=jcienv,
+                depends_on=["JciEnv"],
+            )
+
+        with p.stage("Debug", image=jcienv):
+            p.job(
+                "PublishDocs", f"bash cicd/build_and_publish_docs.sh {p.remote.branch}"
+            )
+
+        if should.release:
+            with p.stage("Publish", image=jcienv):
+                p.job("DockerHubJcienv", "bash cicd/build_and_push_docker.sh jcienv")
+                p.job("DockerHubJci", "bash cicd/build_and_push_docker.sh jci")
+                p.job(
+                    "PublishDocs",
+                    f"bash cicd/build_and_publish_docs.sh {p.remote.branch}",
+                )
+                p.job("PublishPypi", "bash cicd/build_and_push_pypi.sh")
